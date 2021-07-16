5464619a3bfd3fbbd6c7f029835f70591e906e12
Federico Caccia <federicoagustincaccia@gmail.com>
2021-07-16 19:48:14 -0300

    soon

--- components/Soon.js
+++ components/Soon.js
@@ -1,6 +1,6 @@
 function Soon() {
   return (
-    <div className="flex flex-row justify-center items-center bg-gray-dark rounded-2xl p-10 mx-auto my-auto">
+    <div className="flex flex-row justify-center items-center bg-gray-dark rounded-2xl py-6 px-10 mx-auto my-auto">
       <p className="text-gray-lightest titleText">SOON</p>
     </div>
   )
